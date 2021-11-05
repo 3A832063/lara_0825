caba2162a25e22ea1f35c8c0b1afacdd02895ca1
LIN GUAN-YU <kib7410@gmail.com>
2021-11-05 17:20:17 +0800

    '練習1:建立/設定Model'

---
+++ app/Models/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    // 設定 model 對應到的資料表
+    // protected $table = 'posts';
+}
